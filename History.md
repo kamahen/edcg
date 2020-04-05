9f02fcbb0d7863c39d49fa4ce3f471bc49cdfb01
Peter Ludemann <peter.ludemann@gmail.com>
2020-04-05 16:20:07 -0700

    Changed maintainer

--- History.md
+++ History.md
@@ -1,3 +1,7 @@
+# v0.9.0.1 (2020-04-05)
+
+  * Changed maintainer
+
 # v0.9.0 (2015-02-16)
 
   * First release as an SWI-Prolog pack
--- pack.pl
+++ pack.pl
@@ -1,10 +1,11 @@
 name(edcg).
 
 title('Extended DCG').
-version('0.9.0').
-download('https://github.com/mndrix/edcg/archive/v0.9.0.zip').
+version('0.9.0.1').
+keywords([edcg, dcg, accumulator, term_expansion]).
+download('https://github.com/kamahen/edcg/releases/*.tgz').
 
 author('Peter Van Roy','peter.vanroy@uclouvain.be').
 packager('Michael Hendricks','michael@ndrix.org').
-maintainer('Michael Hendricks','michael@ndrix.org').
-home('https://github.com/mndrix/edcg').
+maintainer('Peter Ludemann','peter.ludemann@gmail.com').
+home('https://github.com/kamahen/edcg').
