bdfa434aa365709a03419a113f3f428d3c0c3d0a
Peter Ludemann <peter.ludemann@gmail.com>
2020-04-09 16:06:59 -0700

    Adjust castor/pollux example to match logtalk's; add expr_code example

--- t/examples.pl
+++ t/examples.pl
@@ -6,19 +6,23 @@
 edcg:acc_info(castor,_,_,_,true).
 edcg:acc_info(fwd, T, In, Out, Out=[T|In]). % forward accumulator
 edcg:acc_info(rev, T, Out, In, Out=[T|In]). % reverse accumulator
-edcg:acc_info(adder, I, In, Out, plus(I,In,Out)).  % adder
+edcg:acc_info(adder, I, In, Out, plus(I,In,Out)). % adder
+
+edcg:acc_info(code, T, Out, In, Out=[T|In]).
+edcg:acc_info(size, T, In, Out, Out is In+T).
 
 % Declare passed arguments
 edcg:pass_info(pollux).
 
 % Declare predicates using these hidden arguments
-edcg:pred_info(p,1,[castor,pollux]).  % 1 - X
-edcg:pred_info(q,1,[castor,pollux]).  % 1 - Y
-edcg:pred_info(r,1,[castor,pollux]).  % 1 - Y
+edcg:pred_info(p,1,[castor,pollux]).
+edcg:pred_info(q,1,[castor,pollux]).
+edcg:pred_info(r,1,[castor,pollux]).
 edcg:pred_info(flist,1,[fwd]).
 edcg:pred_info(rlist,1,[rev]).
 edcg:pred_info(sum_first_n,1,[adder]).
 edcg:pred_info(sum,0,[adder,dcg]).
+edcg:pred_info(expr_code,1,[size,code]).
 
 
 % flist(N,[],List) creates the list [1,2,...,N]
@@ -30,6 +34,7 @@ flist(N) -->>
     [N]:fwd,
     N1 is N-1,
     flist(N1).
+
 
 % rlist(N,List,[]) creates the list [N,...,2,1]
 rlist(0) -->>
@@ -62,6 +67,7 @@ sum -->>
 sum -->>
     [].
 
+
 % Program that uses castor and pollux accumulators
 
 p(X) -->>
@@ -69,23 +75,30 @@ p(X) -->>
     q(Y),
     r(Y).
 
-q(Y,_,_,P) :-
-    format('q - Y: ~w, P: ~w~n',[Y,P]).
-r(Y,_,_,P) :-
-    format('r - Y: ~w, P: ~w~n',[Y,P]).
+q(Y) -->>
+    C0/castor,
+    castor/C,
+    P/pollux,
+    { format('q - Y: ~w, C0: ~q, C: ~q, P: ~q~n',[Y,C0,C,P]) },
+    [ ]:castor. % Not needed.
+r(Y) -->>
+    C0/castor/C,
+    P/pollux,
+    { format('r - Y: ~w, C0: ~q, C: ~q, P: ~q~n',[Y,C0,C,P]) }.
 
-% ?- listing(p).
-% p(X, A, B, C) :-
-%     Y is X+1,
-%     q(Y, A, D, C),
-%     r(Y, D, B, C).
 
-% Variables names changed for clarity
-%
-% p(X, Castor_1, Castor_3, Pollux) :-
-%     Y is X+1,
-%     q(Y, Castor_1, Castor_2, Pollux),
-%     r(Y, Castor_2, Castor_3, Pollux).
+
+% Program from the first paper with size, code accumulators.
+
+expr_code(A+B) -->>
+    expr_code(A),
+    expr_code(B),
+    [plus]:code,
+    [1]:size.
+expr_code(I) -->>
+    {atomic(I)},
+    [push(I)]:code,
+    [1]:size.
 
 
 :- use_module(library(plunit)).
@@ -113,9 +126,29 @@ test('sum [2,2,3]') :-
     sum([2,2,3],Sum),
     Sum is 2+2+3.
 
-test(p) :-
+test(p1) :-
     with_output_to(string(Output), p(1,a,a,c)),
-    assertion( Output == "q - Y: 2, P: c\nr - Y: 2, P: c\n" ).
+    assertion( Output == "q - Y: 2, C0: a, C: a, P: c\nr - Y: 2, C0: a, C: a, P: c\n" ).
+
+test(p2) :-
+    with_output_to(string(Output), p(1,a,C2,c)),
+    assertion( C2 == a ),
+    assertion( Output == "q - Y: 2, C0: a, C: a, P: c\nr - Y: 2, C0: a, C: a, P: c\n" ).
+
+% The following example is modified from logtalk's port:
+test(gemini_1) :-
+    with_output_to(string(Output), p(10, S0, S, pollux)),
+    with_output_to(string(S0_str), write(S0)),
+    format(string(Expected),
+           "q - Y: 11, C0: ~s, C: ~s, P: pollux~nr - Y: 11, C0: ~s, C: ~s, P: pollux~n",
+           [S0_str, S0_str, S0_str, S0_str]),
+    assertion( Output == Expected ),
+    assertion( S0 == S ).
+
+test(expr1, [nondet]) :-
+    expr_code((a+b)+(c+d), 0, Size, Code, []),
+    assertion(Size == 7),
+    assertion(Code == [push(a), push(b), plus, push(c), push(d), plus, plus]).
 
 :- end_tests(edcg_examples).
 
